8c6d42a535c642551e7643e61072a4bb1466d7bc
Diego Stallo <dstallo@gmail.com>
2022-11-16 18:35:32 -0300

    Finished with pull.sh script: fully tested and improved logs

--- pull.sh
+++ pull.sh
@@ -9,63 +9,78 @@ SCRIPT_ROOT_DIRECTORY=`dirname "$0"`
 # Include config file
 source $SCRIPT_ROOT_DIRECTORY/config/config.sh
 
-current=`date +%Y%m%d_%H%M`
+# Log functions
+function echolog() {
+    echo "$(date -u): $1" >> $LOG_FILE
+}
 
+function echoerror() {
+    echo "$(date -u): $1" >> $ERROR_FILE
+}
+
+echolog "Backup pulling script started"
+start_time=$SECONDS
+
+# Run through server configuration files on $SERVERS_DIRECTORY.
 for server_file in $SERVERS_DIRECTORY/*.sh; do
     source $server_file
     server_filename=$(basename -- "$server_file")
     server="${server_filename%.*}"
 
-    echo "$current: Downloading $server"
+    echolog "Downloading folder /$FTP_BACKUP_DIRECTORY on server $server"
 
+    # Create folder for server files.
     mkdir -p $TEMP_DIRECTORY/$server
-    cd $TEMP_DIRECTORY/$server
+
+    wget_exit_code=0
     
     # Download every file in server backup directory.
-    wget -r -nv -nH -nc -nd ftp://$FTP_USER:$FTP_PASSWORD@$FTP_HOST/$FTP_BACKUP_DIRECTORY/ 2>&1 | grep -i "failed\|error" 1>&2; 
+    wget_error=$(wget -r -nv -nH -nc -nd -P $TEMP_DIRECTORY/$server ftp://$FTP_USER:$FTP_PASSWORD@$FTP_HOST/$FTP_BACKUP_DIRECTORY/ 2>&1) || wget_exit_code=$?;
     
-    # Get wget exit code to check if there were any problems (although they should already be logged to stderr)
-    wget_exit_code=${PIPESTATUS[0]}
-
+    # Check if wget exit code 
     if [ "$wget_exit_code" -gt 0 ]; then
-        echo "$current: Failed to download backup files from $server. WGET exit code: $get_exit_code"
+        echoerror "Failed to download backup files from $server."
+        echoerror "WGET exit code: $wget_exit_code"
+        echoerror "WGET error message: $wget_error"
         continue;
     fi
 
     download_count=`ls -l | wc -l`
     
+    # Check downloaded file count (alert on 0)
     if [ "$download_count" -eq 0 ]; then
-        echo "$current: No backup files were found at $server. Please check webhosting backup cronjob." 1>&2
+        echoerror "No backup files were found at $server. Please check webhosting backup cronjob."
         continue;
     fi
 
+    # Run for each downloaded file
     for file in $TEMP_DIRECTORY/$server/*; do
-        filesize=$(ls -l $file | awk '{print  $5}')
+        filesize=$(ls -l $file | awk '{print $5}')
         filename=$(basename -- "$file")
+
+        # Check minimum file size
         if [ "$filesize" -lt $MINIMUM_FILESIZE_ERROR ]; then
-            echo "$current: File $filename downloaded from server $server is too small (lower than $MINIMUM_FILESIZE_ERROR bytes). Please check webhosting backup cronjob." 1>&2
+            echoerror "File $filename downloaded from server $server is too small (lower than $MINIMUM_FILESIZE_ERROR bytes). Please check webhosting backup cronjob."
         fi
-        md5=$(md5sum $file)
 
-        echo "$current: Downloaded file $filename from $server. Checksum: $md5"
+        # Get MD5 checksum
+        md5=$(md5sum $file | awk '{print $1}') 2>> $ERROR_FILE
 
-        mv $file $FINAL_BACKUP_DIRECTORY/
+        echolog "Downloaded file $filename with $filesize bytes. Checksum: $md5"
+
+        # Move to final backup destination.
+        cp $file $FINAL_BACKUP_DIRECTORY/ 2>> $ERROR_FILE && rm $file 2>> $ERROR_FILE
+
+        # Delete downloaded files from FTP server if $DELETE_BACKUPS_UPON_PULL is true
+        if [ "$DELETE_BACKUPS_UPON_PULL" = true ] ; then
+            echolog "Deleting file $filename from $server ..."
+            curl --fail --silent --show-error "ftp://$FTP_HOST:$FTP_PORT/" --user "$FTP_USER:$FTP_PASSWORD" -Q "DELE /$FTP_BACKUP_DIRECTORY/$filename" >> /dev/null 2>> $ERROR_FILE && echolog "File $filename successfully deleted on server $server" || echoerror "Failed to delete file $filename on $server"
+        fi
+
     done
 
-    ## Delete downloaded files from FTP server.
+    echolog "Finished with server $server"
+
 done
 
-## Download examples
-    #curl -p - --insecure  "ftp://82.45.34.23:21/CurlPutTest/testfile.xml" --user "testuser:testpassword" -o "C:\test\testfile.xml" --ftp-create-dirs
-    
-    ## Delete
-    #curl -p - --insecure  "ftp://82.45.34.23:21/CurlPutTest/testfile.xml" --user "testuser:testpassword" -Q "–DELE  /CurlPutTest/testfile.xml" --ftp-create-dirs
-
-    #ftp -inv $FTP_HOST <<EOF
-    #    user $FTP_USER $FTP_PASSWORD
-    #    cd $FTP_ROOT/$REMOTE_BACKUP_DIRECTORY
-    #    mget *
-    #    bye
-    #EOF
-    #echo $server//$FTP_HOST:$FTP_PORT/$FTP_USER@$FTP_PASSWORD
-    #wget -r -nH --cut-dirs=5 -nc ftp://user:pass@server//absolute/path/to/directory
+echolog "Backup pulling script finished. It took $(( SECONDS - start_time )) seconds."
